83b7785095da62be613e9b6f23a3d1b936703138
Cristian Araujo <araujocristian@live.com>
2019-09-07 15:28:24 -0300

    Create about page and test link

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = props => {
+  return (
+    <>
+      <h1>About Page</h1>
+      <ul>
+        <li>
+          <Link to="/" activeStyle={{ color: "red" }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" activeStyle={{ color: "red" }}>
+            About
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default AboutPage
