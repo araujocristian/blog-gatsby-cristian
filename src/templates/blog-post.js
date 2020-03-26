bcedb422431445a4147432c157a3c8623cf54926
Cristian Araujo <araujocristian@live.com>
2020-03-26 17:14:52 -0300

    Criando Paginação

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-/* import React from "react"
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const BlogPost = () => {
@@ -25,33 +25,5 @@ const BlogPost = () => {
     </>
   )
 }
-
-export default BlogPost
- */
-
-import React from "react"
-import { graphql } from "gatsby"
-
-const BlogPost = ({ data }) => {
-  const post = data.markdownRemark
-
-  return (
-    <>
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
-  )
-}
-
-export const query = graphql`
-  query Post($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-      }
-      html
-    }
-  }
-`
 
 export default BlogPost
