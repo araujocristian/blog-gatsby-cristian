31b4cc98ac36f0549520298e1b52fd0429198327
Cristian Araujo <araujocristian@live.com>
2020-03-27 13:25:34 -0300

    Configurando o Disqus para comentários

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import RecommendedPosts from "../components/RecommendedPost"
+import Comments from "../components/Comments"
 
 import * as S from "../components/Post/styled"
 
@@ -25,6 +26,7 @@ const BlogPost = ({ data, pageContext }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
       <RecommendedPosts next={nextPost} previous={previousPost} />
+      <Comments url={post.fields.slug} title={post.frontmatter.tittle} />
     </Layout>
   )
 }
@@ -32,6 +34,9 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
