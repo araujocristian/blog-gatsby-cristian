4dcb276ef2e32ae88ae4ad1db310cd7636e2dcb9
Cristian Araujo <araujocristian@live.com>
2020-03-28 23:24:14 -0300

    Add estilos e animações

--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 import { Link } from "gatsby"
 
 export const PostItemLink = styled(Link)`
@@ -28,6 +29,12 @@ export const PostItemWrapper = styled.section`
     flex-direction: column;
     justify-content: center;
   }
+
+  ${media.lessThan("large")`
+    align-items: flex-start;
+    flex-direction: column;
+    padding: 2rem 1rem;
+  `}
 `
 
 export const PostItemTag = styled.div`
@@ -44,6 +51,15 @@ export const PostItemTag = styled.div`
   min-width: 90px;
   text-transform: uppercase;
 
+  ${media.lessThan("large")`
+    border-radius: 0;
+    font-size: 1rem;
+    min-height: auto;
+    min-width: auto;
+    padding: .2rem .5rem;
+    margin-bottom: .7rem;
+  `}
+
   body#grid & {
     margin-bottom: 1.5rem;
   }
@@ -54,10 +70,18 @@ export const PostItemInfo = styled.div`
   flex-direction: column;
   margin-left: 1.5rem;
 
+  ${media.lessThan("large")`
+    margin: 0;
+  `}
+
   body#grid & {
     text-align: center;
     line-height: 1.1;
     margin: 0.8rem 0;
+
+    ${media.lessThan("large")`
+    text-align: unset;
+  `}
   }
 `
 
