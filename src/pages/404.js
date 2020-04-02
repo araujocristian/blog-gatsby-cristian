db074ee1b7093b32be7385716b8376b13a21c64e
Cristian Araujo <araujocristian@live.com>
2020-04-02 09:42:16 -0300

    Add Pagina 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,16 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import { MainContent } from "../components/Post/styled"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found | Cristian Araujo" />
-    <h1>404</h1>
-    <p>Ops... Não tem nada por aqui.</p>
+
+    <MainContent>
+      <h1>404!</h1>
+      <p>Ops... Não tem nada por aqui.</p>
+    </MainContent>
   </Layout>
 )
 
